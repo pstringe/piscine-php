62a7a9699bea3125cf3ce991f10617af2dbbafce
Poitier Stringer <pstringe@e1z3r4p18.42.us.org>
2019-01-09 13:15:46 -0800

    moved ex02 to ex03

---
+++ d01/ex03/ft_split.js
@@ -0,0 +1,18 @@
+function ft_split(s)
+{
+    var split = s.split(' ');
+    return (split);
+}
+
+/*
+** Test
+*/
+
+/*
+var s = "The quick brown fox";
+var split = ft_split(s);
+
+split.forEach(function(e){
+    console.log(e);
+});
+*/
