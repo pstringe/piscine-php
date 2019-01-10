874b58713b6bc1c640605f22855e19d3856fdf4c
Poitier Stringer <pstringe@e1z3r7p15.42.us.org>
2019-01-09 21:02:08 -0800

    ex02 works

---
+++ d01/ex02/oddeven.js
@@ -0,0 +1,30 @@
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   oddeven.js                                         :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2019/01/09 20:27:22 by pstringe          #+#    #+#             //
+//   Updated: 2019/01/09 21:00:39 by pstringe         ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+var oddEven = function () {
+	rl.question('Enter a number: ', (answer) => {
+		if (!isNaN(answer) && answer !== '')
+			console.log(`The number ${answer} is: ${Number(answer) % 2 ? 'odd' : 'even'}`);
+		else
+			console.log(`'${answer}' is not a number`);
+		oddEven();
+	});
+};
+
+oddEven();
