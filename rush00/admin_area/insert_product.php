3b7f236d47b058ac24861ac8f4f560b8a910951b
Poitier Stringer <pstringe@e1z2r10p5.42.us.org>
2019-01-13 04:51:55 -0800

    product form submits, image stored, but file not moved

--- rush00/admin_area/insert_product.php
+++ rush00/admin_area/insert_product.php
@@ -1,37 +1,7 @@
 <!DOCTYPE>
 <?php
 	include("includes/db.php");
-
-	//dynamically retrieve brand names for selection
-	function getBrandSelect()
-	{
-		global $con;
-			
-		$get_brands = "select * from brands";
-		$run_brands = mysqli_query($con, $get_brands);
-		while ($row = mysqli_fetch_array($run_brands)){
-			$id = $row['brand_id'];
-			$title = $row['brand_title'];
-
-			echo "<option value='$id'>$title</option>";
-		}
-	}
-
-	//dynamically retrieve category names for selection
-	function getCategoriesSelect()
-	{
-		global $con;
-			
-		//echo var_dump($con);
-		$get_cats = "select * from categories";
-		$run_cats = mysqli_query($con, $get_cats);
-		while ($row = mysqli_fetch_array($run_cats)){
-			$id = $row['cat_id'];
-			$title =$row['cat_title'];
-
-			echo "<option value='$id'>$title</option>";
-		}
-	}
+	include("../functions/functions.php");
 ?>
 
 <html>
@@ -62,19 +32,42 @@
 					</select>
 				</div>
 				<div class="product_input">
-					Product Image: <input type="text" name="product_image">
+					Product Image: <input type="file" name="product_image">
 				</div>
 				<div class="product_input">
 					Product Price: <input type="text" name="product_price">
 				</div>
 				<div class="product_input">
-					Product Description: <input type="text" name="product_description">
+					Product Description: <textarea name="product_description" cols="20" rows="20"> </textarea>
 				</div>
 				<div class="product_input">
 					Product Keywords: <input type="text" name="product_keywords">
 				</div>
-				<input type="submit" name="insert_post" value="Add Product"/>
+				<input type="submit" name="OK" value="Add Product"/>
 			</form>
+
+			<?php
+				if (isset($_POST['OK'])) {
+					$product_title = $_POST['product_title'];
+					$product_cat = $_POST['product_category'];
+					$product_brand = $_POST['product_brand'];
+					$product_price = $_POST['product_price'];
+					$product_desc = $_POST['product_description'];
+					$product_keywords = $_POST['product_keywords'];
+					$product_image = $_FILES['product_image']['name'];
+					$procuct_image_tmp = $_FILES['product_image']['tmp_name'];
+
+					move_uploaded_file($product_image_tmp, "product_images/$product_image");
+					
+					$insert_product = "insert into products (prod_cat, prod_brand, prod_title, prod_price, prod_desc, prod_img, prod_keywords) values ('$product_cat', '$product_brand', '$product_title','$product_price','$product_desc','$product_image','$product_keywords')";
+					
+					if(($insert = mysqli_query($con, $insert_product))) {
+						echo "<script>alert('Product Sucessfully Added!')</script>";
+						echo "<script>window.open('insert_product.php', '_self')</script>";
+					}
+
+				}
+			?>
 		</div>
 	</body>
 </html>
--- rush00/functions/functions.php
+++ rush00/functions/functions.php
@@ -17,6 +17,7 @@
 		}
 	}
 
+	
 	//dynamically retrieve brands for sidebar 
 	function getBrands()
 	{
@@ -30,6 +31,37 @@
 			$title =$row['brand_title'];
 
 			echo "<div class='brand_link'><a href='#'>$title</a></div>";
+		}
+	}
+
+	//dynamically retrieve brand names for selection
+	function getBrandSelect()
+	{
+		global $con;
+			
+		$get_brands = "select * from brands";
+		$run_brands = mysqli_query($con, $get_brands);
+		while ($row = mysqli_fetch_array($run_brands)){
+			$id = $row['brand_id'];
+			$title = $row['brand_title'];
+
+			echo "<option value='$id'>$title</option>";
+		}
+	}
+
+	//dynamically retrieve category names for selection
+	function getCategoriesSelect()
+	{
+		global $con;
+			
+		//echo var_dump($con);
+		$get_cats = "select * from categories";
+		$run_cats = mysqli_query($con, $get_cats);
+		while ($row = mysqli_fetch_array($run_cats)){
+			$id = $row['cat_id'];
+			$title =$row['cat_title'];
+
+			echo "<option value='$id'>$title</option>";
 		}
 	}
 ?>
