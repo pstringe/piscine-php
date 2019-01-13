42904dc55ecb79e14c136c07fc6e7ffa16619925
Poitier Stringer <pstringe@e1z2r10p5.42.us.org>
2019-01-13 05:18:05 -0800

    values for image var no longer null

--- rush00/admin_area/insert_product.php
+++ rush00/admin_area/insert_product.php
@@ -32,7 +32,8 @@
 					</select>
 				</div>
 				<div class="product_input">
-					Product Image: <input type="file" name="product_image">
+					<!--<input type="hidden" name="MAX_FILE_SIZE" value="30000">-->
+					Product Image: <input type="file" name="product_image" value="">
 				</div>
 				<div class="product_input">
 					Product Price: <input type="text" name="product_price">
@@ -57,15 +58,16 @@
 					$product_image = $_FILES['product_image']['name'];
 					$procuct_image_tmp = $_FILES['product_image']['tmp_name'];
 
-					move_uploaded_file($product_image_tmp, "product_images/$product_image");
+					echo var_dump($produ);
+					if(!move_uploaded_file($product_image_tmp, "product_images/$product_image"))
+						echo "<script>alert(" + var_dump($_FILES) + ")</script>";
 					
 					$insert_product = "insert into products (prod_cat, prod_brand, prod_title, prod_price, prod_desc, prod_img, prod_keywords) values ('$product_cat', '$product_brand', '$product_title','$product_price','$product_desc','$product_image','$product_keywords')";
 					
 					if(($insert = mysqli_query($con, $insert_product))) {
 						echo "<script>alert('Product Sucessfully Added!')</script>";
-						echo "<script>window.open('insert_product.php', '_self')</script>";
+						//echo "<script>window.open('insert_product.php', '_self')</script>";
 					}
-
 				}
 			?>
 		</div>
