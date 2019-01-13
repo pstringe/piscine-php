60d090de061931f71fc613e6edaafea3c56cc419
Poitier Stringer <pstringe@e1z2r10p5.42.us.org>
2019-01-13 07:04:24 -0800

    file upload works

--- rush00/admin_area/insert_product.php
+++ rush00/admin_area/insert_product.php
@@ -10,10 +10,11 @@
 	</head>
 	<body>
 		<div id="product_form">
+			
 			<form action="insert_product.php" method="post" enctype="multipart/form-data">
 				<h2>New Product</h2><br>
 				<div class="product_input">
-					Product Title: <input type="text" name="product_title">
+					Product Title: <input type="text" name="product_title" />
 				</div>
 				<div class="product_input">
 					Product Category: 
@@ -32,11 +33,10 @@
 					</select>
 				</div>
 				<div class="product_input">
-					<!--<input type="hidden" name="MAX_FILE_SIZE" value="30000">-->
-					Product Image: <input type="file" name="product_image" value="">
+					Product Image: <input type="file" name="product_image" />
 				</div>
 				<div class="product_input">
-					Product Price: <input type="text" name="product_price">
+					Product Price: <input type="text" name="product_price" />
 				</div>
 				<div class="product_input">
 					Product Description: <textarea name="product_description" cols="20" rows="20"> </textarea>
@@ -47,7 +47,7 @@
 				<input type="submit" name="OK" value="Add Product"/>
 			</form>
 
-			<?php
+			<?php			
 				if (isset($_POST['OK'])) {
 					$product_title = $_POST['product_title'];
 					$product_cat = $_POST['product_category'];
@@ -55,18 +55,17 @@
 					$product_price = $_POST['product_price'];
 					$product_desc = $_POST['product_description'];
 					$product_keywords = $_POST['product_keywords'];
-					$product_image = $_FILES['product_image']['name'];
-					$procuct_image_tmp = $_FILES['product_image']['tmp_name'];
-
-					echo var_dump($produ);
-					if(!move_uploaded_file($product_image_tmp, "product_images/$product_image"))
-						echo "<script>alert(" + var_dump($_FILES) + ")</script>";
 					
+					$product_image = $_FILES['product_image']['name'];
+					$product_image_tmp = $_FILES['product_image']['tmp_name'];
+
+					move_uploaded_file($product_image_tmp, "product_images/$product_image");
+
 					$insert_product = "insert into products (prod_cat, prod_brand, prod_title, prod_price, prod_desc, prod_img, prod_keywords) values ('$product_cat', '$product_brand', '$product_title','$product_price','$product_desc','$product_image','$product_keywords')";
 					
 					if(($insert = mysqli_query($con, $insert_product))) {
 						echo "<script>alert('Product Sucessfully Added!')</script>";
-						//echo "<script>window.open('insert_product.php', '_self')</script>";
+						echo "<script>window.open('insert_product.php', '_self')</script>";
 					}
 				}
 			?>
