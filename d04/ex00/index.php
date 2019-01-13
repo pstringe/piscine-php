49a1080f0f5c9d5ce2741673d3fee5c2315bb8ed
Poitier Stringer <pstringe@e1z2r12p1.42.us.org>
2019-01-12 22:37:00 -0800

    ex00 works

---
+++ d04/ex00/index.php
@@ -0,0 +1,17 @@
+<?php
+    session_start();
+    if ($_GET['login'] && $_GET['passwd'] && $_GET['submit'] && $_GET['submit'] === "OK"){
+        $_SESSION['login'] = $_GET['login'];
+        $_SESSION['passwd'] = $_GET['passwd'];
+    }
+?>
+<html><body>
+    <form action="index.php" method="GET">
+        login: <input type="text" name="login" value="<?php echo $_SESSION['login']; ?>" />
+        <br />
+        password: <input type="password" name="passwd" value="<?php echo $_SESSION['passwd']; ?>" />
+        <br />
+        <input type="submit" name="submit" value="OK"/>
+    </form>
+</body></html>
+
